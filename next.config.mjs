cd6bece535f6914ca04c07618cb29443d0301d75
Muslim@1446 <m-11105557@moe-dl.edu.my>
2026-03-13 16:07:54 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+// This allows you to use D1, Vectorize, and AI locally when running `npm run dev`
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
